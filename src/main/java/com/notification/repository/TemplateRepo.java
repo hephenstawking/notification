75ae02e8574c34af043d36e9b4aaa9f234527366
hephenstawking <sevenfofive@outlook.com>
2022-10-06 15:43:24 +0300

    added right query and jpa repo

--- src/main/java/com/notification/repository/TemplateRepo.java
+++ src/main/java/com/notification/repository/TemplateRepo.java
@@ -1,14 +1,16 @@
 package com.notification.repository;
 
 import com.notification.entity.NotificationTemplate;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TemplateRepo extends CrudRepository<NotificationTemplate, Integer> {
+public interface TemplateRepo extends JpaRepository<NotificationTemplate, Integer> {
 
+    @Query("SELECT t FROM NotificationTemplate t WHERE t.is_active = 1 AND module = ?1")
     List<NotificationTemplate> getTemplatesByModule(String module);
 
 }
